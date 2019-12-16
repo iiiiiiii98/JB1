3d5884a2c305c3fca751e47de781e4bd68de57b0
iiiiiiii98 <58320580+iiiiiiii98@users.noreply.github.com>
2019-12-16 13:53:00 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//네비게이션
+console.log('hello')
+$(function(){
+    // 마우스 오버시 메뉴 열림
+    $('.depth1 > li').mouseover(function(){
+    // class 추가되면 메뉴 열림(보임)
+        $('.depth2').addClass('show');
+    });
+
+    // 마우스가 나가면 메뉴 닫힘
+    $('.depth1 > li').mouseout(function(){
+        $('.depth2').removeClass('show');
+    });
+
+});
